b04b6b60a07dee9cf7f32cf525cd61734c6a5d88
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-09 15:47:36 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/AppTheme.kt
+++ app/src/main/java/muthopay/muthobrowser/AppTheme.kt
@@ -1,1 +1,13 @@
+package com.muthopay.muthobrowser
 
+import com.muthopay.muthobrowser.preference.IntEnum
+
+/**
+ * The available app themes.
+ */
+enum class AppTheme(override val value: Int) : IntEnum {
+    DEFAULT(0),
+    LIGHT(1),
+    DARK(2),
+    BLACK(3)
+}
