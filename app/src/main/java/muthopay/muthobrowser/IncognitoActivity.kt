ce3d08a9c3642e6039f1260f65c6579cc9715b1b
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-10 12:37:37 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/IncognitoActivity.kt
+++ app/src/main/java/muthopay/muthobrowser/IncognitoActivity.kt
@@ -1,1 +1,50 @@
+package com.muthopay.muthobrowser
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.webkit.CookieManager
+import com.muthopay.muthobrowser.browser.activity.BrowserActivity
+import io.reactivex.Completable
+
+class IncognitoActivity : BrowserActivity() {
+
+    override fun provideThemeOverride(): AppTheme = AppTheme.DARK
+
+    override fun provideAccentThemeOverride(): AccentTheme = AccentTheme.PINK
+
+    @Suppress("DEPRECATION")
+    public override fun updateCookiePreference(): Completable = Completable.fromAction {
+        val cookieManager = CookieManager.getInstance()
+        if (Capabilities.FULL_INCOGNITO.isSupported) {
+            cookieManager.setAcceptCookie(userPreferences.cookiesEnabled)
+        } else {
+            cookieManager.setAcceptCookie(userPreferences.incognitoCookiesEnabled)
+        }
+    }
+
+    @Suppress("RedundantOverride")
+    override fun onNewIntent(intent: Intent) {
+        handleNewIntent(intent)
+        super.onNewIntent(intent)
+    }
+
+    @Suppress("RedundantOverride")
+    override fun onPause() = super.onPause() // saveOpenTabs();
+
+    override fun updateHistory(title: String?, url: String) = Unit // addItemToHistory(title, url)
+
+    override fun isIncognito() = true
+
+    override fun closeActivity() = closePanels(::closeBrowser)
+
+    companion object {
+        /**
+         * Creates the intent with which to launch the activity. Adds the reorder to front flag.
+         */
+        fun createIntent(context: Context, uri: Uri? = null) = Intent(context, IncognitoActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
+            data = uri
+        }
+    }
+}
