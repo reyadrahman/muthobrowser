b9b8460e8052716a6f5b37c6e38b64ae8a93bea1
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-11 16:01:55 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/adblock/NoOpAdBlocker.kt
+++ app/src/main/java/muthopay/muthobrowser/adblock/NoOpAdBlocker.kt
@@ -1,1 +1,14 @@
+package com.muthopay.muthobrowser.adblock
 
+import dagger.Reusable
+import javax.inject.Inject
+
+/**
+ * A no-op ad blocker implementation. Always returns false for [isAd].
+ */
+@Reusable
+class NoOpAdBlocker @Inject constructor() : AdBlocker {
+
+    override fun isAd(url: String) = false
+
+}
