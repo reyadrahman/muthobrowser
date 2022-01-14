e1eb13c2a62aca74359472d553fad9b28749da89
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-14 23:09:33 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/adblock/source/HostsDataSource.kt
+++ app/src/main/java/muthopay/muthobrowser/adblock/source/HostsDataSource.kt
@@ -1,1 +1,20 @@
+package com.muthopay.muthobrowser.adblock.source
 
+import io.reactivex.Single
+
+/**
+ * A data source that contains hosts.
+ */
+interface HostsDataSource {
+
+    /**
+     * Load the hosts and emit them as a [Single] [HostsResult].
+     */
+    fun loadHosts(): Single<HostsResult>
+
+    /**
+     * The unique [String] identifier for this source.
+     */
+    fun identifier(): String
+
+}
