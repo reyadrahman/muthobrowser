642ec17da1daf4f1d0932d2ff8f7d5c49b01d67c
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-16 13:20:07 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/adblock/util/hash/MurmurHashStringAdapter.kt
+++ app/src/main/java/muthopay/muthobrowser/adblock/util/hash/MurmurHashStringAdapter.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.adblock.util.hash
 
+import java.io.Serializable
+
+/**
+ * A [HashingAlgorithm] of type [String] backed by the [MurmurHash].
+ */
+class MurmurHashStringAdapter : HashingAlgorithm<String>, Serializable {
+
+    override fun hash(item: String): Int = MurmurHash.hash32(item)
+
+}
