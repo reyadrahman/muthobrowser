985bd7ca32c91f76201ad7eea2c318bcbd194b54
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-18 14:02:20 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/bookmark/LegacyBookmarkImporter.kt
+++ app/src/main/java/muthopay/muthobrowser/bookmark/LegacyBookmarkImporter.kt
@@ -1,1 +1,17 @@
+package com.muthopay.muthobrowser.bookmark
 
+import com.muthopay.muthobrowser.database.Bookmark
+import com.muthopay.muthobrowser.database.bookmark.BookmarkExporter
+import java.io.InputStream
+import javax.inject.Inject
+
+/**
+ * A [BookmarkImporter] that imports bookmark files that were produced by BookmarkExporter.
+ */
+class LegacyBookmarkImporter @Inject constructor() : BookmarkImporter {
+
+    override fun importBookmarks(inputStream: InputStream): List<Bookmark.Entry> {
+        return BookmarkExporter.importBookmarksFromFileStream(inputStream)
+    }
+
+}
