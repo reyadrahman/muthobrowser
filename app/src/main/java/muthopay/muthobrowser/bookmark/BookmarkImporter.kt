fc457f4644d8ab096d728866597de3253b926621
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-18 14:01:13 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/bookmark/BookmarkImporter.kt
+++ app/src/main/java/muthopay/muthobrowser/bookmark/BookmarkImporter.kt
@@ -1,1 +1,17 @@
+package com.muthopay.muthobrowser.bookmark
 
+import com.muthopay.muthobrowser.database.Bookmark
+import java.io.InputStream
+
+/**
+ * An importer that imports [Bookmark.Entry] from an [InputStream]. Supported formats are details of
+ * the implementation.
+ */
+interface BookmarkImporter {
+
+    /**
+     * Synchronously converts an [InputStream] to a [List] of [Bookmark.Entry].
+     */
+    fun importBookmarks(inputStream: InputStream): List<Bookmark.Entry>
+
+}
