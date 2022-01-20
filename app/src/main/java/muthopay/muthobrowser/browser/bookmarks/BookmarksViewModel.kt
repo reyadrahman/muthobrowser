473111df0b46f14cae1f25abb8f67ffcb901b311
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-20 15:34:20 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/browser/bookmarks/BookmarksViewModel.kt
+++ app/src/main/java/muthopay/muthobrowser/browser/bookmarks/BookmarksViewModel.kt
@@ -1,1 +1,15 @@
+package com.muthopay.muthobrowser.browser.bookmarks
 
+import android.graphics.Bitmap
+import com.muthopay.muthobrowser.database.Bookmark
+
+/**
+ * The data model representing a [Bookmark] in a list.
+ *
+ * @param bookmark The bookmark backing this view model, either an entry or a folder.
+ * @param icon The icon for this bookmark.
+ */
+data class BookmarksViewModel(
+    val bookmark: Bookmark,
+    var icon: Bitmap? = null
+)
