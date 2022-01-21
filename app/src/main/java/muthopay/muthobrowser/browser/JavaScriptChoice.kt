a7d973a71ab4b62fec390aa4795541c369f25b96
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-21 14:07:30 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/browser/JavaScriptChoice.kt
+++ app/src/main/java/muthopay/muthobrowser/browser/JavaScriptChoice.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.browser
 
+import com.muthopay.muthobrowser.preference.IntEnum
+
+/**
+ * The available Block JavaScript choices.
+ */
+enum class JavaScriptChoice(override val value: Int) : IntEnum {
+    NONE(0),
+    WHITELIST(1),
+    BLACKLIST(2)
+}
