8e453d77eaa17db1c83a92bc23973010ea1c031c
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-21 14:08:00 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/browser/BrowserView.kt
+++ app/src/main/java/muthopay/muthobrowser/browser/BrowserView.kt
@@ -1,1 +1,41 @@
+package com.muthopay.muthobrowser.browser
 
+import android.view.View
+import androidx.annotation.StringRes
+import com.muthopay.muthobrowser.ssl.SslState
+
+interface BrowserView {
+
+    fun setTabView(view: View)
+
+    fun removeTabView()
+
+    fun updateUrl(url: String?, isLoading: Boolean)
+
+    fun updateProgress(progress: Int)
+
+    fun updateTabNumber(number: Int)
+
+    fun updateSslState(sslState: SslState)
+
+    fun closeBrowser()
+
+    fun closeActivity()
+
+    fun showBlockedLocalFileDialog(onPositiveClick: () -> Unit)
+
+    fun showSnackbar(@StringRes resource: Int)
+
+    fun setForwardButtonEnabled(enabled: Boolean)
+
+    fun setBackButtonEnabled(enabled: Boolean)
+
+    fun notifyTabViewRemoved(position: Int)
+
+    fun notifyTabViewAdded()
+
+    fun notifyTabViewChanged(position: Int)
+
+    fun notifyTabViewInitialized()
+
+}
