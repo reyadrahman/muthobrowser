89a3214ea2acf83af8bc5bb713d286d7983b8bc4
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-21 14:09:04 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/browser/BookmarksView.kt
+++ app/src/main/java/muthopay/muthobrowser/browser/BookmarksView.kt
@@ -1,1 +1,13 @@
+package com.muthopay.muthobrowser.browser
 
+import com.muthopay.muthobrowser.database.Bookmark
+
+interface BookmarksView {
+
+    fun navigateBack()
+
+    fun handleUpdatedUrl(url: String)
+
+    fun handleBookmarkDeleted(bookmark: Bookmark)
+
+}
