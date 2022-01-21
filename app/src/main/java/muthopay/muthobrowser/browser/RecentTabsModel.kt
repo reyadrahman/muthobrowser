7f686d0133500b2072775eb570dab8db9e706039
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-21 14:06:37 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/browser/RecentTabsModel.kt
+++ app/src/main/java/muthopay/muthobrowser/browser/RecentTabsModel.kt
@@ -1,1 +1,25 @@
+package com.muthopay.muthobrowser.browser
 
+import android.os.Bundle
+import com.muthopay.muthobrowser.extensions.popIfNotEmpty
+import java.util.*
+
+/**
+ * A model that saves [Bundle] and returns the last returned one.
+ */
+class RecentTabsModel {
+
+    val bundleStack: Stack<Bundle> = Stack()
+
+    /**
+     * Return the last closed tab as a [Bundle] or null if there is no previously opened tab.
+     * Removes the [Bundle] from the queue after returning it.
+     */
+    fun popLast(): Bundle? = bundleStack.popIfNotEmpty()
+
+    /**
+     * Add the [savedBundle] to the queue. The next call to [popLast] will return this [Bundle].
+     */
+    fun add(savedBundle: Bundle) = bundleStack.add(savedBundle)
+
+}
