7df51ea82edc69ac3b93346d2eba01ebcdf7ba63
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-22 06:57:27 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/browser/SearchBoxDisplayChoice.kt
+++ app/src/main/java/muthopay/muthobrowser/browser/SearchBoxDisplayChoice.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.browser
 
+import com.muthopay.muthobrowser.preference.IntEnum
+
+/**
+ * An enum representing what detail level should be displayed in the search box.
+ */
+enum class SearchBoxDisplayChoice(override val value: Int) : IntEnum {
+    DOMAIN(0),
+    URL(1),
+    TITLE(2)
+}
