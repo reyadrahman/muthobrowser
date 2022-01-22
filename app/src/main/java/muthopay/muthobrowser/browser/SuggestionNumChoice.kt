978314d408720a8279b5b23b832953fb16830d8f
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-22 06:56:32 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/browser/SuggestionNumChoice.kt
+++ app/src/main/java/muthopay/muthobrowser/browser/SuggestionNumChoice.kt
@@ -1,1 +1,15 @@
+package com.muthopay.muthobrowser.browser
 
+import com.muthopay.muthobrowser.preference.IntEnum
+
+/**
+ * The available Suggestion number choices.
+ */
+enum class SuggestionNumChoice(override val value: Int) : IntEnum {
+    THREE(0),
+    FOUR(1),
+    FIVE(2),
+    SIX(3),
+    SEVEN(4),
+    EIGHT(5)
+}
