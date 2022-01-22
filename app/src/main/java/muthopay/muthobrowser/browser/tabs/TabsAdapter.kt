30e7ab0b6b8725727537ee2b0232f486b30570f3
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-23 01:17:38 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/browser/tabs/TabsAdapter.kt
+++ app/src/main/java/muthopay/muthobrowser/browser/tabs/TabsAdapter.kt
@@ -1,1 +1,67 @@
+package com.muthopay.muthobrowser.browser.tabs
 
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.muthopay.muthobrowser.controller.UIController
+import com.muthopay.muthobrowser.utils.ItemDragDropSwipeAdapter
+import java.util.*
+
+/**
+ * Abstract base tabs adapter.
+ * Implement functionality common to our concrete tabs adapters.
+ */
+abstract class TabsAdapter(val uiController: UIController): RecyclerView.Adapter<TabViewHolder>(), ItemDragDropSwipeAdapter {
+
+    protected var tabList: List<TabViewState> = emptyList()
+
+    /**
+     * Show tabs and compute diffs.
+     */
+    fun showTabs(tabs: List<TabViewState>) {
+        val oldList = tabList
+        tabList = tabs
+        DiffUtil.calculateDiff(TabViewStateDiffCallback(oldList, tabList)).dispatchUpdatesTo(this)
+    }
+
+    /**
+     * From [RecyclerView.Adapter]
+     */
+    override fun getItemCount() = tabList.size
+
+    /**
+     * From [RecyclerView.Adapter]
+     */
+    override fun onViewRecycled(holder: TabViewHolder) {
+        super.onViewRecycled(holder)
+        // I'm not convinced that's needed
+        //(uiController as BrowserActivity).toast("Recycled: " + holder.tab.title)
+        holder.tab = TabViewState()
+    }
+
+    /**
+     * From [ItemDragDropSwipeAdapter]
+     * An item was was moved through drag & drop
+     */
+    override fun onItemMove(fromPosition: Int, toPosition: Int): Boolean
+    {
+        // Note: recent tab list is not affected
+        // Swap local list position
+        Collections.swap(tabList, fromPosition, toPosition)
+        // Swap model list position
+        Collections.swap(uiController.getTabModel().allTabs, fromPosition, toPosition)
+        // Tell base class an item was moved
+        notifyItemMoved(fromPosition, toPosition)
+        return true
+    }
+
+    /**
+     * From [ItemDragDropSwipeAdapter]
+     * An item was was dismissed through swipe
+     */
+    override fun onItemDismiss(position: Int)
+    {
+        uiController.tabCloseClicked(position)
+    }
+
+
+}
