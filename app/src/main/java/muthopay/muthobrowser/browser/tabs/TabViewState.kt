3f07acad2ab2fea357bbe4071f24902935fc475d
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-24 12:44:26 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/browser/tabs/TabViewState.kt
+++ app/src/main/java/muthopay/muthobrowser/browser/tabs/TabViewState.kt
@@ -1,1 +1,30 @@
+package com.muthopay.muthobrowser.browser.tabs
 
+import android.graphics.Bitmap
+import android.graphics.Color
+import com.muthopay.muthobrowser.view.StyxView
+
+/**
+ * @param id The unique id of the tab.
+ * @param title The title of the tab.
+ * @param favicon The favicon of the tab, may be null.
+ */
+data class TabViewState(
+        val id: Int = 0,
+        val title: String = "",
+        val favicon: Bitmap? = null,
+        val isForeground: Boolean = false,
+        val themeColor: Int = Color.TRANSPARENT,
+        val isFrozen: Boolean = true
+)
+
+/**
+ * Converts a [StyxView] to a [TabViewState].
+ */
+fun StyxView.asTabViewState() = TabViewState(
+    id = id,
+    title = title,
+    favicon = favicon,
+    isForeground = isForeground,
+    themeColor = htmlMetaThemeColor
+)
