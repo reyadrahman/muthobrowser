30edd474cbab44af87d792062526656c609b19d7
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-25 14:41:54 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/browser/sessions/SessionsAdapter.kt
+++ app/src/main/java/muthopay/muthobrowser/browser/sessions/SessionsAdapter.kt
@@ -1,1 +1,131 @@
+package com.muthopay.muthobrowser.browser.sessions
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.TextViewCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.muthopay.muthobrowser.R
+import com.muthopay.muthobrowser.controller.UIController
+import com.muthopay.muthobrowser.extensions.inflater
+import com.muthopay.muthobrowser.utils.ItemDragDropSwipeAdapter
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.BehaviorSubject
+import java.util.*
+
+/**
+ * Sessions [RecyclerView.Adapter].
+ */
+class SessionsAdapter(
+        private val uiController: UIController
+) : RecyclerView.Adapter<SessionViewHolder>(), ItemDragDropSwipeAdapter {
+
+    // Current sessions shown in our dialog
+    private var iSessions: ArrayList<Session> = arrayListOf()
+    // Collection of disposable subscriptions observing edit mode changes
+    private var iEditModeSubscriptions: CompositeDisposable = CompositeDisposable()
+    // Here comes some most certainly overkill way to notify our view holders that our edit mode has changed
+    // See: https://medium.com/@MiguelSesma/update-recycler-view-content-without-refreshing-the-data-bb79d768bde8
+    // See: https://stackoverflow.com/a/49433976/3969362
+    var iEditModeEnabledObservable = BehaviorSubject.createDefault(false)
+
+    /**
+     * Display the given list of session in our recycler view.
+     * Possibly updating an existing list.
+     */
+    fun showSessions(aSessions: List<Session>) {
+        DiffUtil.calculateDiff(SessionsDiffCallback(iSessions, aSessions)).dispatchUpdatesTo(this)
+        iSessions.clear()
+        // Do a deep copy for our diff to work
+        aSessions.forEach { s -> iSessions.add(Session(s.name,s.tabCount,s.isCurrent)) }
+    }
+
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView)
+        // Taking some wild guess here
+        // Do we need to explicitly call that when adapter is being destroyed?
+        // I'm guessing not
+        iEditModeSubscriptions.dispose()
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): SessionViewHolder {
+        val view = viewGroup.context.inflater.inflate(R.layout.session_list_item, viewGroup, false)
+        return SessionViewHolder(view, uiController).apply {
+            // Ask our newly created view holder to observe our edit mode status
+            // Thus buttons on our items will be shown or hidden
+            iEditModeSubscriptions.add(observeEditMode(iEditModeEnabledObservable))
+        }
+    }
+
+    override fun onBindViewHolder(holder: SessionViewHolder, position: Int) {
+        val session = iSessions[position]
+        holder.textName.tag = session.name
+        holder.textName.text = session.name
+        holder.textTabCount.text = holder.tabCountLabel()
+
+        if (iEditModeEnabledObservable.value == true) {
+            holder.buttonEdit.visibility = View.VISIBLE
+            holder.buttonDelete.visibility = View.VISIBLE
+        } else {
+            holder.buttonEdit.visibility = View.GONE
+            holder.buttonDelete.visibility = View.GONE
+        }
+
+        // Set item font style according to current session
+        if (session.isCurrent) {
+            TextViewCompat.setTextAppearance(holder.textName, R.style.boldText)
+            holder.iCardView.isChecked = true
+        } else {
+            TextViewCompat.setTextAppearance(holder.textName, R.style.normalText)
+            holder.iCardView.isChecked = false
+        }
+    }
+
+    override fun getItemCount() = iSessions.size
+
+    // From ItemTouchHelperAdapter
+    // An item was was moved through drag & drop
+    override fun onItemMove(fromPosition: Int, toPosition: Int): Boolean
+    {
+        // Note: recent tab list is not affected
+        // Swap local list position
+        Collections.swap(iSessions, fromPosition, toPosition)
+        // Swap model list position
+        Collections.swap(uiController.getTabModel().iSessions, fromPosition, toPosition)
+        // Tell base class an item was moved
+        notifyItemMoved(fromPosition, toPosition)
+        // Persist our changes
+        uiController.getTabModel().saveSessions()
+        return true
+    }
+
+    // From ItemTouchHelperAdapter
+    override fun onItemDismiss(position: Int)
+    {
+
+    }
+
+}
+
+
+/**
+ * Diffing callback used to determine whether changes have been made to the list.
+ *
+ * @param oldList The old list that is being replaced by the [newList].
+ * @param newList The new list replacing the [oldList], which may or may not be different.
+ */
+class SessionsDiffCallback(
+        private val oldList: List<Session>,
+        private val newList: List<Session>
+) : DiffUtil.Callback() {
+    override fun getOldListSize() = oldList.size
+
+    override fun getNewListSize() = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+            oldList[oldItemPosition].name == newList[newItemPosition].name
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+            oldList[oldItemPosition].tabCount == newList[newItemPosition].tabCount &&
+            oldList[oldItemPosition].isCurrent == newList[newItemPosition].isCurrent
+}
