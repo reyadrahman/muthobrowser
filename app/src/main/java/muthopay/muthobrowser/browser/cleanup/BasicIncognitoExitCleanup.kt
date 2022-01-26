fe7b94086a9bf022ca468fed1a268efa2c0b43b2
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-26 13:42:22 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/browser/cleanup/BasicIncognitoExitCleanup.kt
+++ app/src/main/java/muthopay/muthobrowser/browser/cleanup/BasicIncognitoExitCleanup.kt
@@ -1,1 +1,18 @@
-/
+package com.muthopay.muthobrowser.browser.cleanup
+
+import android.webkit.WebView
+import com.muthopay.muthobrowser.browser.activity.BrowserActivity
+import com.muthopay.muthobrowser.utils.WebUtils
+import javax.inject.Inject
+
+/**
+ * Exit cleanup that should run on API < 28 when the incognito instance is closed. This is
+ * significantly less secure than on API > 28 since we can separate WebView data from
+ */
+class BasicIncognitoExitCleanup @Inject constructor() : ExitCleanup {
+    override fun cleanUp(webView: WebView?, context: BrowserActivity) {
+        // We want to make sure incognito mode is secure as possible without also breaking existing
+        // browser instances.
+        WebUtils.clearWebStorage()
+    }
+}
