8d33064c3cd768735e51696f925b0ad78b918664
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-26 13:41:30 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/browser/cleanup/DelegatingExitCleanup.kt
+++ app/src/main/java/muthopay/muthobrowser/browser/cleanup/DelegatingExitCleanup.kt
@@ -1,1 +1,26 @@
+package com.muthopay.muthobrowser.browser.cleanup
 
+import android.webkit.WebView
+import com.muthopay.muthobrowser.Capabilities
+import com.muthopay.muthobrowser.MainActivity
+import com.muthopay.muthobrowser.browser.activity.BrowserActivity
+import com.muthopay.muthobrowser.isSupported
+import javax.inject.Inject
+
+/**
+ * Exit cleanup that determines which sort of cleanup to do at runtime. It determines which cleanup
+ * to perform based on the API version and whether we are in incognito mode or normal mode.
+ */
+class DelegatingExitCleanup @Inject constructor(
+    private val basicIncognitoExitCleanup: BasicIncognitoExitCleanup,
+    private val enhancedIncognitoExitCleanup: EnhancedIncognitoExitCleanup,
+    private val normalExitCleanup: NormalExitCleanup
+) : ExitCleanup {
+    override fun cleanUp(webView: WebView?, context: BrowserActivity) {
+        when {
+            context is MainActivity -> normalExitCleanup.cleanUp(webView, context)
+            Capabilities.FULL_INCOGNITO.isSupported -> enhancedIncognitoExitCleanup.cleanUp(webView, context)
+            else -> basicIncognitoExitCleanup.cleanUp(webView, context)
+        }
+    }
+}
