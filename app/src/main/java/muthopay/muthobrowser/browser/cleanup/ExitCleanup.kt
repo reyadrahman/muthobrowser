7bc54c69fa415250cf186640f1c6f240ce754d53
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-27 15:20:06 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/browser/cleanup/ExitCleanup.kt
+++ app/src/main/java/muthopay/muthobrowser/browser/cleanup/ExitCleanup.kt
@@ -1,1 +1,18 @@
+package com.muthopay.muthobrowser.browser.cleanup
 
+import android.webkit.WebView
+import com.muthopay.muthobrowser.browser.activity.BrowserActivity
+
+/**
+ * A command that runs as the browser instance is shutting down to clean up anything that needs to
+ * be cleaned up. For instance, if the user has chosen to clear cache on exit or if incognito mode
+ * is closing.
+ */
+interface ExitCleanup {
+
+    /**
+     * Clean up the instance of the browser with the provided [webView] and [context].
+     */
+    fun cleanUp(webView: WebView?, context: BrowserActivity)
+
+}
