6e07df16eb4bdba2dda47fb203d31a3cd6acbdbc
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-27 15:18:48 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/browser/cleanup/NormalExitCleanup.kt
+++ app/src/main/java/muthopay/muthobrowser/browser/cleanup/NormalExitCleanup.kt
@@ -1,1 +1,44 @@
+package com.muthopay.muthobrowser.browser.cleanup
 
+import android.webkit.WebView
+import com.muthopay.muthobrowser.browser.activity.BrowserActivity
+import com.muthopay.muthobrowser.database.history.HistoryDatabase
+import com.muthopay.muthobrowser.di.DatabaseScheduler
+import com.muthopay.muthobrowser.log.Logger
+import com.muthopay.muthobrowser.preference.UserPreferences
+import com.muthopay.muthobrowser.utils.WebUtils
+import io.reactivex.Scheduler
+import javax.inject.Inject
+
+/**
+ * Exit cleanup that should run whenever the main browser process is exiting.
+ */
+class NormalExitCleanup @Inject constructor(
+    private val userPreferences: UserPreferences,
+    private val logger: Logger,
+    private val historyDatabase: HistoryDatabase,
+    @DatabaseScheduler private val databaseScheduler: Scheduler
+) : ExitCleanup {
+    override fun cleanUp(webView: WebView?, context: BrowserActivity) {
+        if (userPreferences.clearCacheExit) {
+            WebUtils.clearCache(webView, context)
+            logger.log(TAG, "Cache Cleared")
+        }
+        if (userPreferences.clearHistoryExitEnabled) {
+            WebUtils.clearHistory(context, historyDatabase, databaseScheduler)
+            logger.log(TAG, "History Cleared")
+        }
+        if (userPreferences.clearCookiesExitEnabled) {
+            WebUtils.clearCookies()
+            logger.log(TAG, "Cookies Cleared")
+        }
+        if (userPreferences.clearWebStorageExitEnabled) {
+            WebUtils.clearWebStorage()
+            logger.log(TAG, "WebStorage Cleared")
+        }
+    }
+
+    companion object {
+        const val TAG = "NormalExitCleanup"
+    }
+}
