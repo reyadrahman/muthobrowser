49e66c24b026fa821d8a78aba5eee3a23e88e697
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-28 12:41:29 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/database/adblock/Host.kt
+++ app/src/main/java/muthopay/muthobrowser/database/adblock/Host.kt
@@ -1,1 +1,9 @@
+package com.muthopay.muthobrowser.database.adblock
 
+/**
+ * Representation of a host.
+ *
+ * @param name The name of the host.
+ */
+@JvmInline
+value class Host(val name: String)
