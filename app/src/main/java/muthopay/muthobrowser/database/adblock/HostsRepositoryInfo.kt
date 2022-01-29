e796906d3a5df6894a6a73a4c09ede1acb9acb93
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-01-29 13:05:55 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/database/adblock/HostsRepositoryInfo.kt
+++ app/src/main/java/muthopay/muthobrowser/database/adblock/HostsRepositoryInfo.kt
@@ -1,1 +1,20 @@
+package com.muthopay.muthobrowser.database.adblock
+
+import android.content.SharedPreferences
+import com.muthopay.muthobrowser.R
+import com.muthopay.muthobrowser.di.AdBlockPrefs
+import com.muthopay.muthobrowser.preference.delegates.nullableStringPreference
+import javax.inject.Inject
+
+/**
+ * Information about the contents of the hosts repository.
+ */
+class HostsRepositoryInfo @Inject constructor(@AdBlockPrefs preferences: SharedPreferences) {
+
+    /**
+     * The identity of the contents of the hosts repository as a [String] or `null`.
+     */
+    var identity: String? by preferences.nullableStringPreference(R.string.pref_key_identity)
+
+}
 
