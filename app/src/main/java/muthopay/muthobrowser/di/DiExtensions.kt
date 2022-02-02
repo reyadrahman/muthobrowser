6c78cbe6a9975090b84ab0ef0da0abc42cba694d
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-02 13:52:15 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/di/DiExtensions.kt
+++ app/src/main/java/muthopay/muthobrowser/di/DiExtensions.kt
@@ -1,1 +1,21 @@
+@file:JvmName("Injector")
+
+package com.muthopay.muthobrowser.di
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import com.muthopay.muthobrowser.BrowserApp
+
+/**
+ * The [AppComponent] attached to the application [Context].
+ */
+val Context.injector: AppComponent
+    get() = (applicationContext as BrowserApp).applicationComponent
+
+/**
+ * The [AppComponent] attached to the context, note that the fragment must be attached.
+ */
+val Fragment.injector: AppComponent
+    get() = (context?.applicationContext as BrowserApp).applicationComponent
+
 
