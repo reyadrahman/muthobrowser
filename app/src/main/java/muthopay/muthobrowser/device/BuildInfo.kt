c584478191e30742280827587d946223050af30b
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-02 13:49:07 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/device/BuildInfo.kt
+++ app/src/main/java/muthopay/muthobrowser/device/BuildInfo.kt
@@ -1,1 +1,14 @@
+package com.muthopay.muthobrowser.device
 
+/**
+ * A representation of the info for the current build.
+ */
+data class BuildInfo(val buildType: BuildType)
+
+/**
+ * The types of builds that this instance of the app could be.
+ */
+enum class BuildType {
+    DEBUG,
+    RELEASE
+}
