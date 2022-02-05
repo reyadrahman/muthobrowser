2c9cf5ed16c73d36c642c88279fbd5c0edfdad1d
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-05 17:39:17 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/extensions/CloseableExtensions.kt
+++ app/src/main/java/muthopay/muthobrowser/extensions/CloseableExtensions.kt
@@ -1,1 +1,16 @@
+package com.muthopay.muthobrowser.extensions
 
+import android.util.Log
+import java.io.Closeable
+
+/**
+ * Close a [Closeable] and absorb any exceptions within [block], logging them when they occur.
+ */
+inline fun <T : Closeable, R> T.safeUse(block: (T) -> R): R? {
+    return try {
+        this.use(block)
+    } catch (throwable: Throwable) {
+        Log.e("Closeable", "Unable to parse results", throwable)
+        null
+    }
+}
