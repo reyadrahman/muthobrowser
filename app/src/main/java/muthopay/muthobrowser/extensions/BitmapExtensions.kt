f0177d08bc3824db002f712ce17179a0ba2927d8
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-05 17:40:26 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/extensions/BitmapExtensions.kt
+++ app/src/main/java/muthopay/muthobrowser/extensions/BitmapExtensions.kt
@@ -1,1 +1,27 @@
+package com.muthopay.muthobrowser.extensions
 
+import android.graphics.*
+import androidx.core.graphics.createBitmap
+
+/**
+ * Creates and returns a new favicon which is the same as the provided favicon but with horizontal
+ * and vertical padding of 4dp
+ *
+ * @return the padded bitmap.
+ */
+fun Bitmap.pad(): Bitmap {
+    return this
+}
+
+private val desaturatedPaint = Paint().apply {
+    colorFilter = ColorMatrixColorFilter(ColorMatrix().apply {
+        setSaturation(0.5f)
+    })
+}
+
+/**
+ * Desaturates a [Bitmap] to 50% grayscale. Note that a new bitmap will be created.
+ */
+fun Bitmap.desaturate(): Bitmap = createBitmap(width, height).also {
+    Canvas(it).drawBitmap(this, 0f, 0f, desaturatedPaint)
+}
