b99731ce8581a50004b042b3b0957dfaa4099ad5
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-06 13:16:39 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/extensions/JSONArrayExtensions.kt
+++ app/src/main/java/muthopay/muthobrowser/extensions/JSONArrayExtensions.kt
@@ -1,1 +1,8 @@
+package com.muthopay.muthobrowser.extensions
 
+import org.json.JSONArray
+
+/**
+ * Map each item in a [JSONArray] to a list of a new type.
+ */
+inline fun <T> JSONArray.map(map: (Any) -> T): List<T> = (0 until length()).map { map(this[it]) }
