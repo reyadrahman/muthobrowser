7fc4c2a3711b81a9be01d5971625d5d8d1ef3b28
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-08 12:49:37 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/html/download/DownloadPageFactory.kt
+++ app/src/main/java/muthopay/muthobrowser/html/download/DownloadPageFactory.kt
@@ -1,1 +1,75 @@
+package com.muthopay.muthobrowser.html.download
 
+import android.app.Application
+import com.muthopay.muthobrowser.R
+import com.muthopay.muthobrowser.constant.FILE
+import com.muthopay.muthobrowser.database.downloads.DownloadEntry
+import com.muthopay.muthobrowser.database.downloads.DownloadsRepository
+import com.muthopay.muthobrowser.html.HtmlPageFactory
+import com.muthopay.muthobrowser.html.ListPageReader
+import com.muthopay.muthobrowser.html.jsoup.*
+import com.muthopay.muthobrowser.preference.UserPreferences
+import dagger.Reusable
+import io.reactivex.Single
+import java.io.File
+import java.io.FileWriter
+import javax.inject.Inject
+
+/**
+ * The factory for the downloads page.
+ */
+@Reusable
+class DownloadPageFactory @Inject constructor(
+    private val application: Application,
+    private val userPreferences: UserPreferences,
+    private val manager: DownloadsRepository,
+    private val listPageReader: ListPageReader
+) : HtmlPageFactory {
+
+    override fun buildPage(): Single<String> = manager
+        .getAllDownloads()
+        .map { list ->
+            parse(listPageReader.provideHtml()) andBuild {
+                title { application.getString(R.string.action_downloads) }
+                body {
+                    val repeatableElement = id("repeated").removeElement()
+                    id("content") {
+                        list.forEach {
+                            appendChild(repeatableElement.clone {
+                                tag("a") { attr("href", createFileUrl(it.title)) }
+                                id("title") { text(createFileTitle(it)) }
+                                id("url") { text(it.url) }
+                            })
+                        }
+                    }
+                }
+            }
+        }
+        .map { content -> Pair(createDownloadsPageFile(), content) }
+        .doOnSuccess { (page, content) ->
+            FileWriter(page, false).use { it.write(content) }
+        }
+        .map { (page, _) -> "$FILE$page" }
+
+
+    private fun createDownloadsPageFile(): File = File(application.filesDir, FILENAME)
+
+    private fun createFileUrl(fileName: String): String = "$FILE${userPreferences.downloadDirectory}/$fileName"
+
+    private fun createFileTitle(downloadItem: DownloadEntry): String {
+        val contentSize = if (downloadItem.contentSize.isNotBlank()) {
+            "[${downloadItem.contentSize}]"
+        } else {
+            ""
+        }
+
+        return "${downloadItem.title} $contentSize"
+    }
+
+    companion object {
+
+        const val FILENAME = "downloads.html"
+
+    }
+
+}
