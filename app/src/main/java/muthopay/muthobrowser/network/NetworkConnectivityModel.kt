a14b9088e2c2667686a00a63ccfbe6bbad7f00b6
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-12 10:16:56 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/network/NetworkConnectivityModel.kt
+++ app/src/main/java/muthopay/muthobrowser/network/NetworkConnectivityModel.kt
@@ -1,1 +1,33 @@
+package com.muthopay.muthobrowser.network
 
+import android.app.Application
+import android.net.ConnectivityManager
+import com.muthopay.muthobrowser.rx.BroadcastReceiverObservable
+import dagger.Reusable
+import io.reactivex.Observable
+import javax.inject.Inject
+
+/**
+ * A model that supplies network connectivity status updates.
+ */
+@Reusable
+class NetworkConnectivityModel @Inject constructor(
+    private val connectivityManager: ConnectivityManager,
+    private val application: Application
+) {
+
+    /**
+     * An infinite observable that emits a boolean value whenever the network condition changes.
+     * Emitted value is true when the network is in the connected state, and it is false otherwise.
+     */
+    @Suppress("DEPRECATION")
+    fun connectivity(): Observable<Boolean> = BroadcastReceiverObservable(
+        NETWORK_BROADCAST_ACTION,
+        application
+    ).map { connectivityManager.activeNetworkInfo?.isConnected == true }
+
+    companion object {
+        private const val NETWORK_BROADCAST_ACTION = "android.net.conn.CONNECTIVITY_CHANGE"
+    }
+
+}
