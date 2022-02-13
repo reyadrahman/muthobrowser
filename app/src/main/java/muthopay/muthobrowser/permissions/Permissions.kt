8f009dcaef99fa90e855eef69288d0c889a17d8e
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-13 21:17:31 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/permissions/Permissions.kt
+++ app/src/main/java/muthopay/muthobrowser/permissions/Permissions.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.permissions
 
+/**
+ * Enum class to handle the different states
+ * of permissions since the PackageManager only
+ * has a granted and denied state.
+ */
+enum class Permissions {
+    GRANTED,
+    DENIED,
+    NOT_FOUND
+}
