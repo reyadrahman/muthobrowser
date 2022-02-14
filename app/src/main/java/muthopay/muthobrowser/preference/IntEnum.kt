254e8f6a04b71b874175db08821e4ca2f13ad009
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-14 11:54:38 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/preference/IntEnum.kt
+++ app/src/main/java/muthopay/muthobrowser/preference/IntEnum.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.preference
 
+/**
+ * An enum value that has an integer representation.
+ */
+interface IntEnum {
+
+    /**
+     * The integer representation of the enum value.
+     */
+    val value: Int
+}
