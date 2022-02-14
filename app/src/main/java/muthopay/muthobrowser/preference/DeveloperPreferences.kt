97c3982b06f0cf5b7ff00c8fbfb1321a743a1fbe
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-14 11:54:49 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/preference/DeveloperPreferences.kt
+++ app/src/main/java/muthopay/muthobrowser/preference/DeveloperPreferences.kt
@@ -1,1 +1,28 @@
+package com.muthopay.muthobrowser.preference
 
+import android.content.SharedPreferences
+import com.muthopay.muthobrowser.di.DevPrefs
+import com.muthopay.muthobrowser.preference.delegates.booleanPreference
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Preferences related to development debugging.
+ *
+ * Created by anthonycr on 2/19/18.
+ */
+@Singleton
+class DeveloperPreferences @Inject constructor(
+    @DevPrefs preferences: SharedPreferences
+) {
+
+    var useLeakCanary by preferences.booleanPreference(LEAK_CANARY, false)
+
+    var checkedForTor by preferences.booleanPreference(INITIAL_CHECK_FOR_TOR, false)
+
+    var checkedForI2P by preferences.booleanPreference(INITIAL_CHECK_FOR_I2P, false)
+}
+
+private const val LEAK_CANARY = "leakCanary"
+private const val INITIAL_CHECK_FOR_TOR = "checkForTor"
+private const val INITIAL_CHECK_FOR_I2P = "checkForI2P"
