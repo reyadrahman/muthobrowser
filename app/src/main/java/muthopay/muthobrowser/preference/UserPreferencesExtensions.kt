75a26f8eece58ba90586cea3e5ace93ab5de15b3
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-14 11:54:06 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/preference/UserPreferencesExtensions.kt
+++ app/src/main/java/muthopay/muthobrowser/preference/UserPreferencesExtensions.kt
@@ -1,1 +1,29 @@
+package com.muthopay.muthobrowser.preference
 
+import android.app.Application
+import android.webkit.WebSettings
+import com.muthopay.muthobrowser.constant.*
+
+/**
+ * Return the user agent chosen by the user or the custom user agent entered by the user.
+ */
+fun UserPreferences.userAgent(application: Application): String =
+    when (val choice = userAgentChoice) {
+        // WebSettings default identifies us as WebView and as WebView Google is preventing us to login to its services.
+        // Clearly we don't want that so we just modify default user agent by removing the WebView specific parts.
+        // That should make us look like Chrome, which we are really.
+        1 -> {
+              var userAgent = Regex(" Build/.+; wv").replace(WebSettings.getDefaultUserAgent(application),"")
+              userAgent = Regex("Version/.+? ").replace(userAgent,"")
+              userAgent
+        }
+        2 -> WINDOWS_DESKTOP_USER_AGENT
+        3 -> LINUX_DESKTOP_USER_AGENT
+        4 -> MACOS_DESKTOP_USER_AGENT
+        5 -> ANDROID_MOBILE_USER_AGENT
+        6 -> IOS_MOBILE_USER_AGENT
+        7 -> System.getProperty("http.agent") ?: " "
+        8 -> WebSettings.getDefaultUserAgent(application)
+        9 -> userAgentString.takeIf(String::isNotEmpty) ?: " "
+        else -> throw UnsupportedOperationException("Unknown userAgentChoice: $choice")
+    }
