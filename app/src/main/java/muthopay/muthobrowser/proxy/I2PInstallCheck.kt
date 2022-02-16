b0bb0f489cf2ddaf87d6276cbe0f2d99501b1cd5
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-16 12:56:00 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/proxy/I2PInstallCheck.kt
+++ app/src/main/java/muthopay/muthobrowser/proxy/I2PInstallCheck.kt
@@ -1,1 +1,30 @@
+package com.muthopay.muthobrowser.proxy
 
+import android.content.Context
+import android.content.pm.PackageManager
+
+class I2PInstallCheck(private val mContext: Context) {
+    private val mUseDebug = false
+    val isI2PAndroidInstalled: Boolean
+        get() = mUseDebug && isAppInstalled(URI_I2P_ANDROID_DEBUG) ||
+                isAppInstalled(URI_I2P_ANDROID) ||
+                isAppInstalled(URI_I2P_ANDROID_DONATE) ||
+                isAppInstalled(URI_I2P_ANDROID_LEGACY)
+
+    private fun isAppInstalled(uri: String): Boolean {
+        val pm = mContext.packageManager
+        return try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES)
+            true
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
+    }
+
+    companion object {
+        const val URI_I2P_ANDROID = "net.i2p.android"
+        const val URI_I2P_ANDROID_DONATE = "net.i2p.android.donate"
+        const val URI_I2P_ANDROID_LEGACY = "net.i2p.android.router"
+        const val URI_I2P_ANDROID_DEBUG = "net.i2p.android.debug"
+    }
+}
