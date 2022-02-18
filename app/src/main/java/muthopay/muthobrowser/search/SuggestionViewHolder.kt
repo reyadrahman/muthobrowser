6001e4349839aa4c9d22713c8e869e1b28b293e5
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-18 08:47:05 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/SuggestionViewHolder.kt
+++ app/src/main/java/muthopay/muthobrowser/search/SuggestionViewHolder.kt
@@ -1,1 +1,13 @@
+package com.muthopay.muthobrowser.search
 
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.muthopay.muthobrowser.R
+
+class SuggestionViewHolder(view: View) {
+    val imageView: ImageView = view.findViewById(R.id.suggestionIcon)
+    val titleView: TextView = view.findViewById(R.id.title)
+    val urlView: TextView = view.findViewById(R.id.url)
+    val insertSuggestion: View = view.findViewById(R.id.complete_search)
+}
