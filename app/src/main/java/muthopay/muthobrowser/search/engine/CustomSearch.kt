1a8236a1f13fe53829970e478bc0884e392a52ea
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-19 12:50:39 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/CustomSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/CustomSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * A custom search engine.
+ */
+class CustomSearch(queryUrl: String) : BaseSearchEngine(
+    "file:///android_asset/styx.webp",
+    queryUrl,
+    R.string.search_engine_custom
+)
