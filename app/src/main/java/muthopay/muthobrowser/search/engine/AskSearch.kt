3ddb047b1cf654c2191bfe22dfcc64b8eec575d5
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-19 12:53:34 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/AskSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/AskSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The Ask search engine.
+ */
+class AskSearch : BaseSearchEngine(
+    "file:///android_asset/ask.webp",
+    "http://www.ask.com/web?qsrc=0&o=0&l=dir&qo=StyxBrowser&q=",
+    R.string.search_engine_ask
+)
