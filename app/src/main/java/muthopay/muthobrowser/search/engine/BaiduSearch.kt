6d980eb86884dd105f64910574a50a5282f51974
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-19 12:52:11 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/BaiduSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/BaiduSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The Baidu search engine.
+ */
+class BaiduSearch : BaseSearchEngine(
+    "file:///android_asset/baidu.webp",
+    "https://www.baidu.com/s?wd=",
+    R.string.search_engine_baidu
+)
