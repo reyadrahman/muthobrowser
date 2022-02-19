e6c5d4484aacfdde3941fb2b382d5d84d68f28d9
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-19 12:51:17 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/BingSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/BingSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The Bing search engine.
+ */
+class BingSearch : BaseSearchEngine(
+    "file:///android_asset/bing.webp",
+    "https://www.bing.com/search?q=",
+    R.string.search_engine_bing
+)
