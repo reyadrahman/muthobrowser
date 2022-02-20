2ce1ed06c91354afd8127f1eb9eb58f5ca94af0d
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-20 12:22:27 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/DuckLiteSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/DuckLiteSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The DuckDuckGo Lite search engine.
+ */
+class DuckLiteSearch : BaseSearchEngine(
+    "file:///android_asset/duckduckgo.webp",
+    "https://duckduckgo.com/lite/?t=styx&q=",
+    R.string.search_engine_duckduckgo_lite
+)
