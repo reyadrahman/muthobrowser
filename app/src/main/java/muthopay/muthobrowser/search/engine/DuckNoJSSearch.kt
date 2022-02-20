52613ecd56e11b29adcfd2a0b37365700ac1c00f
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-20 12:22:15 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/DuckNoJSSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/DuckNoJSSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The DuckDuckGo search engine.
+ */
+class DuckNoJSSearch : BaseSearchEngine(
+    "file:///android_asset/duckduckgo.webp",
+    "https://duckduckgo.com/html/?q=",
+    R.string.search_engine_duckduckgo_no_js
+)
