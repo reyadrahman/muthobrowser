bafb8fb174d9ce80be52773afffb170b63a555f9
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-21 11:20:24 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/EcosiaSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/EcosiaSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The Ecosia search engine.
+ */
+class EcosiaSearch : BaseSearchEngine(
+    "file:///android_asset/ecosia.webp",
+    "https://www.ecosia.org/search?q=",
+    R.string.search_engine_ecosia
+)
