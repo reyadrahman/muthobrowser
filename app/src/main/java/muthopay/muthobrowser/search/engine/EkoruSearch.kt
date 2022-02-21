e8cfdae1065bab4ce2693761f1fd159841155553
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-21 11:19:40 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/EkoruSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/EkoruSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The Ekoru search engine.
+ */
+class EkoruSearch : BaseSearchEngine(
+    "file:///android_asset/ekoru.webp",
+    "https://www.ekoru.org/?ext=styx&q=",
+    R.string.search_engine_ekoru
+)
