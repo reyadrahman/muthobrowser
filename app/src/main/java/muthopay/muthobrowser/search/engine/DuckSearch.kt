757d1541af1519581f4c73fe9ee9ed355fded13b
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-21 11:20:55 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/DuckSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/DuckSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The DuckDuckGo search engine.
+ */
+class DuckSearch : BaseSearchEngine(
+    "file:///android_asset/duckduckgo.webp",
+    "https://duckduckgo.com/?t=styx&q=",
+    R.string.search_engine_duckduckgo
+)
