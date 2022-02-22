543a8f54dd4cc8a1576bc5831bc7f89d5090ca1b
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-22 12:43:20 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/MojeekSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/MojeekSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The Mojeek search engine.
+ */
+class MojeekSearch : BaseSearchEngine(
+    "file:///android_asset/mojeek.webp",
+    "https://www.mojeek.com/search?q=",
+    R.string.search_engine_mojeek
+)
