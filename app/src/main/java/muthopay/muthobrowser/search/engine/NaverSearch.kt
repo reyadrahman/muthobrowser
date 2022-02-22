cce8f3e66f2e23a2dab0e6fd5a8573d6af5c2e9d
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-22 12:42:58 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/NaverSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/NaverSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The Naver search engine.
+ */
+class NaverSearch : BaseSearchEngine(
+    "file:///android_asset/naver.webp",
+    "https://search.naver.com/search.naver?ie=utf8&query=",
+    R.string.search_engine_naver
+)
