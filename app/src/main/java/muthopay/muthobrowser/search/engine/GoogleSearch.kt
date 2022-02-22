3a3ba20bcd421338988c206a7c4357118d0fd592
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-22 12:44:01 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/GoogleSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/GoogleSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The Google search engine.
+ */
+class GoogleSearch : BaseSearchEngine(
+    "file:///android_asset/google.webp",
+    "https://www.google.com/search?client=styx&ie=UTF-8&oe=UTF-8&q=",
+    R.string.search_engine_google
+)
