e422477af3b500991e250f6facaf0be66fedea9a
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-23 22:51:12 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/StartPageSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/StartPageSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The StartPage search engine.
+ */
+class StartPageSearch : BaseSearchEngine(
+    "file:///android_asset/startpage.webp",
+    "https://startpage.com/do/search?language=english&query=",
+    R.string.search_engine_startpage
+)
