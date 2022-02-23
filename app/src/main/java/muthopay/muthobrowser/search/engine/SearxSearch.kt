dab5ca839231a9c255e09e2b452900664ff1cc46
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-23 22:52:01 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/SearxSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/SearxSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The Searx search engine.
+ */
+class SearxSearch : BaseSearchEngine(
+    "file:///android_asset/searx.webp",
+    "https://searx.prvcy.eu/search?q=",
+    R.string.search_engine_searx
+)
