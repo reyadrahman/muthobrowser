0e73b8f73aa08c7fbc548be8b99b2655e79623a3
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-23 22:52:21 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/QwantSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/QwantSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The Qwant search engine.
+ */
+class QwantSearch : BaseSearchEngine(
+    "file:///android_asset/qwant.webp",
+    "https://www.qwant.com/?q=",
+    R.string.search_engine_qwant
+)
