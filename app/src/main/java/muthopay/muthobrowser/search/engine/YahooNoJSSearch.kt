6fe124d08e3f2f3c36e4aafe9de25579066402b1
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-24 14:16:30 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/YahooNoJSSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/YahooNoJSSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The Yahoo search engine.
+ */
+class YahooNoJSSearch : BaseSearchEngine(
+    "file:///android_asset/yahoo.webp",
+    "https://search.yahoo.com/mobile/s?nojs=1&p=",
+    R.string.search_engine_yahoo_no_js
+)
