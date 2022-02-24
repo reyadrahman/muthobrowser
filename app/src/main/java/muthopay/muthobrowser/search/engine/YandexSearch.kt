220c56a90307f3a9ce03aaaceed6ccba485376d4
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-24 14:14:09 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/engine/YandexSearch.kt
+++ app/src/main/java/muthopay/muthobrowser/search/engine/YandexSearch.kt
@@ -1,1 +1,12 @@
+package com.muthopay.muthobrowser.search.engine
 
+import com.muthopay.muthobrowser.R
+
+/**
+ * The Yandex search engine.
+ */
+class YandexSearch : BaseSearchEngine(
+    "file:///android_asset/yandex.webp",
+    "https://yandex.ru/yandsearch?lr=21411&text=",
+    R.string.search_engine_yandex
+)
