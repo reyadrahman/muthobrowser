6eeccc080d06d425c672cc3c9bbc01ef08bd0e4c
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-25 06:28:09 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/suggestions/DuckSuggestionsModel.kt
+++ app/src/main/java/muthopay/muthobrowser/search/suggestions/DuckSuggestionsModel.kt
@@ -1,1 +1,47 @@
+package com.muthopay.muthobrowser.search.suggestions
 
+import android.app.Application
+import com.muthopay.muthobrowser.R
+import com.muthopay.muthobrowser.constant.UTF8
+import com.muthopay.muthobrowser.database.SearchSuggestion
+import com.muthopay.muthobrowser.extensions.map
+import com.muthopay.muthobrowser.extensions.preferredLocale
+import com.muthopay.muthobrowser.log.Logger
+import com.muthopay.muthobrowser.preference.UserPreferences
+import io.reactivex.Single
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import org.json.JSONArray
+import org.json.JSONObject
+
+/**
+ * The search suggestions provider for the DuckDuckGo search engine.
+ */
+class DuckSuggestionsModel(
+    okHttpClient: Single<OkHttpClient>,
+    requestFactory: RequestFactory,
+    application: Application,
+    logger: Logger,
+    userPreferences: UserPreferences
+) : BaseSuggestionsModel(okHttpClient, requestFactory, UTF8, application.preferredLocale, logger, userPreferences) {
+
+    private val searchSubtitle = application.getString(R.string.suggestion)
+
+    // https://duckduckgo.com/ac/?q={query}
+    override fun createQueryUrl(query: String, language: String): HttpUrl = HttpUrl.Builder()
+        .scheme("https")
+        .host("duckduckgo.com")
+        .encodedPath("/ac/")
+        .addEncodedQueryParameter("q", query)
+        .build()
+
+    @Throws(Exception::class)
+    override fun parseResults(responseBody: ResponseBody): List<SearchSuggestion> {
+        return JSONArray(responseBody.string())
+            .map { it as JSONObject }
+            .map { it.getString("phrase") }
+            .map { SearchSuggestion("$searchSubtitle \"$it\"", it) }
+    }
+
+}
