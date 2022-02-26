70500810c8c23eba6a9afa57144a042a34f977c4
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-26 23:49:02 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/suggestions/SuggestionsRepository.kt
+++ app/src/main/java/muthopay/muthobrowser/search/suggestions/SuggestionsRepository.kt
@@ -1,1 +1,19 @@
+package com.muthopay.muthobrowser.search.suggestions
 
+import com.muthopay.muthobrowser.database.SearchSuggestion
+import io.reactivex.Single
+
+/**
+ * A repository for search suggestions.
+ */
+interface SuggestionsRepository {
+
+    /**
+     * Creates a [Single] that fetches the search suggestion results for the provided query.
+     *
+     * @param rawQuery the raw query to retrieve the results for.
+     * @return a [Single] that emits the list of results for the query.
+     */
+    fun resultsForSearch(rawQuery: String): Single<List<SearchSuggestion>>
+
+}
