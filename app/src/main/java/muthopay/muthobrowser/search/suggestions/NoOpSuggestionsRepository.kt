eda556a51770a80371e46f9d5dad1d2882388546
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-02-26 23:49:34 +0600

    Add files via upload

--- app/src/main/java/muthopay/muthobrowser/search/suggestions/NoOpSuggestionsRepository.kt
+++ app/src/main/java/muthopay/muthobrowser/search/suggestions/NoOpSuggestionsRepository.kt
@@ -1,1 +1,14 @@
+package com.muthopay.muthobrowser.search.suggestions
 
+import com.muthopay.muthobrowser.database.SearchSuggestion
+import io.reactivex.Single
+
+/**
+ * A search suggestions repository that doesn't fetch any results.
+ */
+class NoOpSuggestionsRepository : SuggestionsRepository {
+
+    private val emptySingle: Single<List<SearchSuggestion>> = Single.just(emptyList())
+
+    override fun resultsForSearch(rawQuery: String) = emptySingle
+}
